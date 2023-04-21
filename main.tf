883e95d94d6ebd8e2ead391205f6a26ea584fc7e
devenderchohan <chohandevender@gmail.com>
2023-04-20 23:19:49 -0400

    removing the provider from the module

--- main.tf
+++ main.tf
@@ -6,12 +6,7 @@ terraform {
     }
   }
 }
-provider "aws" {
 
-    region = "${var.region}"
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-}
 data "aws_availability_zones" "available" {
   state = "available"
 }
